ed14378fe44d203910211f966480a0a5feab604c
Damien Cassou <damien@cassou.me>
2024-04-03 11:50:30 +0200

    32129139: Setup jest

---
+++ jest.config.mjs
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+	// Automatically clear mock calls, instances, contexts and results
+	// before every test:
+	clearMocks: true,
+
+	// Indicates whether the coverage information should be collected
+	// while executing the test:
+	collectCoverage: true,
+
+	// An array of glob patterns indicating a set of files for which
+	// coverage information should be collected:
+	collectCoverageFrom: ["./src/**"],
+
+	// The directory where Jest should output its coverage files:
+	coverageDirectory: "coverage",
+};
+
+export default config;
